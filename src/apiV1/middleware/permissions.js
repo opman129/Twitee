f71f4c0f74f26fd69cf284f4682e25b4573a5929
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-12-08 18:42:02 +0100

    comment middleware

--- src/apiV1/middleware/permissions.js
+++ src/apiV1/middleware/permissions.js
@@ -3,6 +3,7 @@ const { catchAsync } = require('../../utils/catchAsync');
 const Post = require('../models/posts/post.model');
 const Comment = require('../models/comments/comment.model');
 
+/** Middleware to check if user is the creator of a post */
 exports.checkIfUserIsPostCreator = catchAsync( async (req, res, next) => {
     const query = { _id: req.params.post_id };
     const post = await Post.findOne(query);
@@ -16,6 +17,7 @@ exports.checkIfUserIsPostCreator = catchAsync( async (req, res, next) => {
     return next(new AppError("You do not have the correct permissions to carry out this action", 403));
 });
 
+/** Middleware to check if user is the creator of a comment */
 exports.checkIfUserIsCommentCreator = catchAsync (async (req, res, next) => {
     const query = { _id: req.params.comment_id };
     const comment = await Comment.findOne(query);
